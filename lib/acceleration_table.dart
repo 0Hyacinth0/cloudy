62b33b903bc0acf847a91785b7e2380b6d9afdae
hyacinth <905149301@qq.com>
2023-06-09 10:49:32 +0800

    update20230609

---
+++ lib/acceleration_table.dart
@@ -0,0 +1,68 @@
+// acceleration_table.dart
+class AccelerationTable {
+  // 表格数据
+  final Map<String, List<Map<String, dynamic>>> data = {
+  "gcd": [
+  {"time": 1.50, "level": 0, "rate": 0.0},
+  {"time": 1.44, "level": 95, "rate": 0.1},
+  {"time": 1.38, "level": 4241, "rate": 4.4},
+  {"time": 1.31, "level": 8857, "rate": 9.18},
+  {"time": 1.25, "level": 13851, "rate": 14.36},
+  {"time": 1.19, "level": 19316, "rate": 20.02}
+  ],
+  "huiXuePiaoYaoGuiZi": [
+  {"time": 2.44, "level": 0, "rate": 0.0},
+  {"time": 2.25, "level": 95, "rate": 0.1},
+  {"time": 2.06, "level": 8104, "rate": 8.4},
+  {"time": 1.88, "level": 17620, "rate": 18.26}
+  ],
+  "huiXuePiaoYaoYuHanYingRi": [
+  {"time": 1.88, "level": 0, "rate": 0.0},
+  {"time": 1.69, "level": 9705, "rate": 10.06},
+  {"time": 1.50, "level": 23650, "rate": 24.51}
+  ],
+  "zuoXuanYouZhuan": [
+  {"time": 5.00, "level": 0, "rate": 0.0},
+  {"time": 4.69, "level": 95, "rate": 0.1},
+  {"time": 4.38, "level": 6502, "rate": 6.74},
+  {"time": 4.06, "level": 13851, "rate": 14.36},
+  {"time": 3.75, "level": 22331, "rate": 23.14}
+  ]
+  };
+
+  // 根据加速等级计算加速段数和技能引导时间
+  Map<String, dynamic> calculate(int speedLevel, String skill) {
+  List<Map<String, dynamic>> skillData = data[skill];
+  if (skillData == null) {
+    return {"error": "Invalid skill name"};
+  }
+
+  Map<String, dynamic> result = {"time": 0.0, "level": 0, "rate": 0.0};
+  for (int i = 0; i < skillData.length; i++) {
+    if (speedLevel >= skillData[i]["level"]) {
+      result = skillData[i];
+    } else {
+      break;
+    }
+  }
+
+  return result;
+}
+void main() {
+  AccelerationTable table = new AccelerationTable();
+// 示例：用户填写的加速等级为 10000，查询左旋右转技能
+  int userSpeedLevel = 10000;
+  String skillName = "zuoXuanYouZhuan";
+  Map<String, dynamic> result = table.calculate(userSpeedLevel, skillName);
+  if (result.containsKey("error")) {
+    print(result["error"]);
+  } else {
+    print("技能引导时间: ${result['time']}s");
+    print("加速段数: ${result['level']}");
+    print("加速率: ${result['rate']}%");
+  }
+}
+
+}
+
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-// 导入 Flutter 相关的包
 import 'package:flutter/material.dart';
+import 'acceleration_table.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,18 +9,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '云裳心经手法计算器',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: '云裳心经手法计算器'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
+  MyHomePage({required Key key, this.title}) : super(key: key);
   final String title;
 
   @override
@@ -30,47 +30,91 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController _speedLevelController = TextEditingController();
 
-  void _calculate() {
-    // 获取输入的加速等级
-    int speedLevel = int.tryParse(_speedLevelController.text);
-
-    if (speedLevel == null) {
-      // 处理无效输入
-      print('输入无效');
-      return;
+  void calculate() {
+    int userSpeedLevel = int.tryParse(speedInputController.text);
+    if (userSpeedLevel == null || userSpeedLevel < 0) {
+      setState(() {
+        result = {"error": "Please enter a valid speed level"};
+      });
+    } else {
+      result = table.calculate(userSpeedLevel, selectedSkill);
+      setState(() {});
     }
-
-    // 在此处执行计算操作
-    print('加速等级: $speedLevel');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _speedLevelController,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: speedInputController,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: '加速等级',
+                  labelText: 'Speed Level',
+                  hintText: 'Enter your speed level',
                   border: OutlineInputBorder(),
                 ),
               ),
-            ),
-            RaisedButton(
-              onPressed: _calculate,
-              child: Text('查看结果'),
-            ),
-          ],
+              SizedBox(height: 10),
+              DropdownButtonFormField<String>(
+                value: selectedSkill,
+                onChanged: (String newValue) {
+                  setState(() {
+                    selectedSkill = newValue;
+                  });
+                },
+                items: <String>[
+                  'zuoXuanYouZhuan',
+                  'quXianKaiKong',
+                  'ruanZhuanJiShi'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                decoration: InputDecoration(
+                  labelText: 'Select Skill',
+                  hintText: 'Choose a skill',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  calculate();
+                },
+                child: Text('Calculate'),
+              ),
+
+              // 根据计算结果显示信息
+              if (result != null) ...[
+                SizedBox(height: 20),
+                if (result.containsKey("error")) ...[
+                  Text(
+                    result["error"],
+                    style: TextStyle(color: Colors.red, fontSize: 16),
+                  )
+                ] else ...[
+                  Text('技能引导时间: ${result['time']}s'),
+                  Text('加速段数: ${result['level']}'),
+                  Text('加速率: ${result['rate']}%'),
+                ],
+              ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
 
